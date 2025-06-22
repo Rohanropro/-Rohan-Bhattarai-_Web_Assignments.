b27c80a5cb157c08566072fb9dd2839f1fda8145
Rohanropro <122427118+Rohanropro@users.noreply.github.com>
2025-06-22 17:33:40 +0545

    Update script.js

--- Final Exam/todo/script.js
+++ Final Exam/todo/script.js
@@ -1,8 +1,8 @@
 (function () {
-  // TODO 1: Create names array
+
   var names = ["Alice", "Bob", "Charlie", "jsRaj", "JSNina", "Sam"];
 
-  // Loop through names
+
   for (var i = 0; i < names.length; i++) {
     var name = names[i];
     var firstLetter = name.charAt(0).toLowerCase();
